76c3adcbc4f5bf20a6bbb58455c43e98f8b38166
Jason Stumpf <jason.stumpf@gmail.com>
2017-01-19 16:35:52 -0800

    Added initial normalization manager.

---
+++ normalized.hh
@@ -0,0 +1,17 @@
+#ifndef INCLUDED_NORMALIZED_HH
+#define INCLUDED_NORMALIZED_HH
+
+//! A Normalized<T, f> is a T that has been normalized by normalize.
+template <typename T, T (&normalize)(T const &)>
+class Normalized {
+  T val;
+
+public:
+  Normalized() : val(normalize(T())) {}
+  Normalized(T const & t) : val(normalize(t)) {}
+  T const & operator*() const { return val; }
+  T const * operator->() const { return &val; }
+  operator T const &() const { return val; }
+};
+
+#endif
